97c907cd7f56b6a29288e18393a37496df670980
pedfog <pedram.fog2@gmail.com>
2022-06-15 01:00:30 +0430

    Configured ESLint and installed its dependencies, Added Tailwind and react-router-dom to the project

---
+++ src/App.tsx
@@ -0,0 +1,10 @@
+import { ReactElement } from 'react';
+import { Routes, Route } from 'react-router-dom';
+
+export default function App(): ReactElement {
+  return (
+    <Routes>
+      
+    </Routes>
+  );
+}
--- src/index.tsx
+++ src/index.tsx
@@ -1,14 +1,18 @@
-import React from 'react';
+import { StrictMode } from 'react';
 import ReactDOM from 'react-dom/client';
+import { BrowserRouter } from 'react-router-dom';
 import reportWebVitals from './reportWebVitals';
+import App from './App';
 
 const root = ReactDOM.createRoot(
-  document.getElementById('root') as HTMLElement
+  document.getElementById('root') as HTMLElement,
 );
 root.render(
-  <React.StrictMode>
-
-  </React.StrictMode>
+  <StrictMode>
+    <BrowserRouter>
+      <App />
+    </BrowserRouter>
+  </StrictMode>,
 );
 
 // If you want to start measuring performance in your app, pass a function
