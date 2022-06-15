8704de3abd102b78d74097cc1729af44bad19f93
pedfog <pedram.fog2@gmail.com>
2022-06-15 23:12:47 +0430

    Created page for homePage and components folder, fixed an ESLint warning

--- src/App.tsx
+++ src/App.tsx
@@ -1,10 +1,13 @@
 import { ReactElement } from 'react';
 import { Routes, Route } from 'react-router-dom';
+import HomePage from './pages/Home';
+import NotFoundPage from './pages/404Page';
 
 export default function App(): ReactElement {
   return (
     <Routes>
-
+      <Route path="/" element={<HomePage />} />
+      <Route path="*" element={<NotFoundPage />} />
     </Routes>
   );
 }
