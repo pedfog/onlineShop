850c9b3359118c7d0bb032fdb8285c9d7bb6eb86
pedfog <pedram.fog2@gmail.com>
2022-06-18 20:43:23 +0430

    Built component for cart items table and rendered saved products of localStorage into that

--- src/components/shoppingCart/index.tsx
+++ src/components/shoppingCart/index.tsx
@@ -1,9 +1,28 @@
 import { ReactElement } from 'react';
+import SingleTableRow from './SingleTableRow';
+import { Product } from '../products/productType';
 
 export default function ShoppingCart(): ReactElement {
+  const cartItems = JSON.parse(localStorage.getItem('cartItems') || '[]') as Product[];
+
   return (
-    <>
-      ShoppingCart
-    </>
+    <table className="table-auto w-full mt-8 border-2">
+      <thead>
+        <tr>
+          <th>Number</th>
+          <th>Title</th>
+          <th>Description</th>
+          <th>Quantity</th>
+          <th>Price</th>
+          <th>Total Price</th>
+          <th>{''}</th>
+        </tr>
+      </thead>
+      <tbody>
+        {(cartItems && cartItems[0]) ? cartItems.map((item, index) => (
+          <SingleTableRow key={item.id} product={item} index={index} />
+        )) : null}
+      </tbody>
+    </table>
   );
 }
